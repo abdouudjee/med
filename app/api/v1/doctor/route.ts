ec3067002bdb9f880de89b7eec54844c649b439e
abdouudjee <djedouanimohamedabdallah@gmail.com>
2026-02-09 09:42:10 +0100

    test3

---
+++ app/api/v1/doctor/route.ts
@@ -0,0 +1,7 @@
+import { doctors } from "@/app/db/schema";
+import { db } from "@/app/db/index";
+
+export async function GET(request: Request) {
+    const res = await db.select().from(doctors);
+    return Response.json({ doctors: res })
+}
--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,6 +1,20 @@
 import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+
+
+
 export const users = pgTable('users', {
     id: serial('id').primaryKey(),
     fullName: text('full_name'),
     phone: varchar('phone', { length: 256 }),
+    email: varchar('email', { length: 256 }),
+    password: varchar('password', { length: 256 })
 });
+
+export const doctors = pgTable('doctors', {
+    id: serial('id').primaryKey(),
+    phone: varchar('phone', { length: 256 }),
+    email: varchar('email', { length: 256 }),
+    password: varchar('password', { length: 256 })
+})
+export const appointment = pgTable('appointment',{
+})
