0b4acb3bea750af49447d8b8b6d061940c6a2395
abdouudjee <djedouanimohamedabdallah@gmail.com>
2026-02-09 08:47:36 +0100

    test2

--- app/api/v1/test/route.ts
+++ app/api/v1/test/route.ts
@@ -1,3 +1,6 @@
+import { users } from "@/app/db/schema";
+import { db } from "@/app/db/index"
 export async function GET(request: Request) {
-    return Response.json({ test: 'save value to secure store...' })
+    const res = await db.select().from(users);
+    return Response.json({ test: 'save value to secure store...', res })
 }
